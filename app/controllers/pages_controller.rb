54ff83277d57bbeee6ffdb53efd2766c384af7c4
Hugo Daniel <daniel.hugo@protonmail.com>
2020-11-11 14:16:49 +0100

    Add MJ

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,13 +4,17 @@ class PagesController < ApplicationController
   end
 
   def home
-    @current_character = Character.find(params[:character_id])
-    @characters = Character.all
-                           .order(:id)
-                           .includes(:special_skills)
-                           .where.not(id: params[:character_id])
-                           .to_a
-                           .unshift(@current_character)
+    if params[:character_id]
+      @current_character = Character.find(params[:character_id])
+      @characters = Character.all
+                             .order(:id)
+                             .includes(:special_skills)
+                             .where.not(id: params[:character_id])
+                             .to_a
+                             .unshift(@current_character)
+    else
+      @characters = Character.all
+    end
     @messages ||= Message.all.order("created_at DESC")
     @maps = Map.with_attached_image
   end
